7afe7bd2f0a0f0829ad2a530dfe783119e227736
carl <segui.carl@gmail.com>
2026-04-26 17:05:33 +0800

    update dashboard test module and add a new file called CartPage.ts

--- src/pages/CartPage.ts
+++ src/pages/CartPage.ts
@@ -1,9 +1,18 @@
 import { BasePage } from "./BasePage";
 import {ELEMENT_WAIT, SHORT_WAIT} from '../utils/timeout';
 import {Page, Locator, expect} from "@playwright/test";
-import { listenerCount } from "node:cluster";
+
 
 export class CartPage extends BasePage{
+    
+    
+    //cart page elements
+        readonly cartItemName: Locator;
 
+    constructor(page: Page){
+        super(page);
+        this.cartItemName = page.locator('.cart_item_name');    
 
-}
+    };
+
+};
